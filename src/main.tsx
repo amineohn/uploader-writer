d19664a0b61cfbea8071c84374aa1333b54c4bb3
Amine <amineprojet7@gmail.com>
2022-08-30 09:52:38 +0200

    chore: init app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Home } from './pages/home'
+import './styles/app.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="bg-white overflow-hidden shadow rounded-lg">
+              <div className="px-4 py-5 sm:p-6 text-neutral-500">hey</div>
+          </div>
+      </div>
+  )
+}
